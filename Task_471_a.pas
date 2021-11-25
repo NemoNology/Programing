1f37ae76e77e77b443f97d26421d1506d6415eb6
NemoNology <91414886+NemoNology@users.noreply.github.com>
2021-11-25 18:10:05 +0900

    V1
    V1: рабочая программа

---
+++ Task_471_a.pas
@@ -0,0 +1,31 @@
+﻿program Task_471_a;
+
+
+var
+
+  sum, a : real;
+
+  f : Text;
+
+begin
+
+  Assign(f, 'file.txt');                  // Assign(f, 'f.txt') - связать файл
+                                          //
+                                          // Reset(f) - Чтение файла & Его открытие
+  Reset(f);                               // Rewrite(f) - Запись в файл & Его открытие
+                                          // 
+                                          // Close(f) - "Закрыть" файл
+  while not Eof(f) do
+    begin
+    
+    readln(f, a);
+    sum := sum + a;                                      
+    
+    end;
+
+  Close(f);
+  
+  writeln('Полученная сумма: ', sum);
+  
+
+end.
