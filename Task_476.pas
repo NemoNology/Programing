e16c66cba21df84266716470000b6d53b45e588c
NemoNology <91414886+NemoNology@users.noreply.github.com>
2021-11-25 18:22:44 +0900

    V1
    V1: готовая программа

---
+++ Task_476.pas
@@ -0,0 +1,33 @@
+﻿program Task_476;
+
+
+var
+
+  f_char, g : Text;
+  
+  temp : char;
+
+begin
+
+
+  Assign(f_char, 'f_char.txt');
+  Assign(g, 'g.txt');
+   
+  Reset(f_char);
+  Rewrite(g);
+  
+  
+  while not Eof(f_char) do
+    begin
+    
+      readln(f_char, temp);
+      writeln(g, temp);
+    
+    end;  
+ 
+ 
+  Close(f_char);
+  Close(g);
+   
+
+end.
