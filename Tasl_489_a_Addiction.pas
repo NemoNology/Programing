299327e01389689e97b468b8c47f3e813c85ef29
NemoNology <91414886+NemoNology@users.noreply.github.com>
2021-11-27 12:05:57 +0900

    V1: дополнение для задания 489 (а)
    V1: дополнение для задания 489 (а). Создание случайных чисел типа integer для файла f_integer для последующей работы с ним

---
+++ Tasl_489_a_Addiction.pas
@@ -0,0 +1,48 @@
+﻿program Task_486;
+
+
+procedure Ran_486(f : Text);
+
+var
+
+  i : integer;
+
+begin
+  
+  
+  
+  for i := 1 to 10 do
+    
+    writeln(f, random(100));
+  
+  for i := 1 to 10 do
+  
+    writeln(f, random(100) * -1);
+  
+end;
+
+
+
+
+
+var
+
+  f_integer, g : Text;
+
+begin
+
+
+  Assign(f_integer, 'f_integer.txt');
+  Assign(g, 'g.txt');
+ 
+  
+  Rewrite(f_integer);
+  
+  
+  Ran_486(f_integer);
+  Ran_486(f_integer);
+  
+  
+  Close(f_integer);
+  
+end.
