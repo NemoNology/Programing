73856be24590cef77350cdc128738bed9f0b804b
NemoNology <91414886+NemoNology@users.noreply.github.com>
2021-11-29 23:23:23 +0900

    V1.2
    V1.2: добавлены комментарии

--- Task_531.pas
+++ Task_531.pas
@@ -3,26 +3,24 @@
 
 var 
 
-  i, n : integer;
+  i, n : integer;                             // n - вводимое значение n, i - счётчик
   
-  x : Array of integer;
+  x : Array of integer;                       // Массив для переменных а
   
 begin
 
   
-  readln(n);
+  readln(n);                                  // Получаем размерность будущего массива
+  
+  x := new integer[n];                        // Задаём новую размерность массива
 
-  x := new integer[n];
-
+  for i := 0 to n - 1 do      
+    
+    x[i] := random(100);                      // Заполняем массив случайными переменными
+        
   for i := 0 to n - 1 do
     
-    x[i] := random(100);
-      
-  for i := 0 to n - 1 do
+    write(x[i] - x[n - 1], '; ');             // Выводим результат программы
     
-    write(x[i] - x[n - 1], '; ');
-    
-    writeln();
-
 
 end.
