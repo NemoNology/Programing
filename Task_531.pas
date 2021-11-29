2fd79a2804631849c063d6567479e91dd5ade11c
NemoNology <91414886+NemoNology@users.noreply.github.com>
2021-11-29 23:00:14 +0900

    V1: сделано через метод random
    V1: готовое задание

---
+++ Task_531.pas
@@ -0,0 +1,28 @@
+﻿program Task_519_a;
+
+
+var 
+
+  i, n : integer;
+  
+  x : Array of integer;
+  
+begin
+
+  
+  readln(n);
+
+  x := new integer[n];
+
+  for i := 0 to n - 1 do
+    
+    x[i] := random(100);
+      
+  for i := 0 to n - 1 do
+    
+    write(x[i] - x[n - 1], '; ');
+    
+    writeln();
+
+
+end.
